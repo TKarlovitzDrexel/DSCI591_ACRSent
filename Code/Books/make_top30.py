9cc407d023d0ef6cbb41c00fd805b0f12530a5a1
Tessa Karlovitz <tk597@drexel.edu>
2021-05-20 18:13:18 -0400

    Updated the python file for comparable dataset and jupyter notebook for the part 2 ranking portion of the books

--- Code/Books/make_top30.py
+++ Code/Books/make_top30.py
@@ -21,50 +21,18 @@ f.write(outline)
 third_pass = pd.read_csv('../../3rdpass_cleaned.tsv', sep='\t', quoting=csv.QUOTE_NONE)
 
 first30 = third_pass.groupby('Title').tail(30)
+first30_grouped = first30.groupby('Title')
 
-current_title = ''
 count_lines = 0
-for index, row in first30.iterrows():
+for title, group in first30_grouped:
+    outline = title
+    for index, row in group.iterrows():
+        outline += "\t" + row['Review_Date'] + "\t" + row['Review_Body']
     count_lines += 1
-    title = row[0]
-    if title == current_title: # add review to the current line
-        outline += "\t" + row[4] + "\t" + row[3]
-    else: # write out the current line and start a new line 
-        outline += "\n" 
-        f.write(outline)
-        outline = title + "\t" + row[4] + "\t" + row[3]
-    if count_lines > 300: 
-        f.close()
-        print(zz)
-
-# Read review counts and get list of good titles (this is the dict from the 2nd cleaning pass) 
-good_titles_file = '2ndpass_reviewcounts_30.tsv'
-good_titles = []
-
-with open(good_titles_file) as file_in:
-    # Skip the header line
-    next(file_in)
-    # Loop through each line in the file
-    for line in file_in:
-        # Split the lines on tabs
-        line_data = line.split('\t')
-        # Append title from good_title_file line to good_titles list 
-        good_titles.append(line_data[0])
-
-print(len(good_titles))
-good_titles_set = set(good_titles) #making it a set to save time with checking if elements are in the set
-
-# Read in the first pass cleaned tsv
-file_name = '1stpass_cleaned.tsv'
-
-with open(file_name) as file_in2:
-    # Skip the header line
-    next(file_in2)
-    # Loop through each line in the file
-    for line in file_in2:
-        # Split the lines on tabs
-        line_data = line.split('\t')
-        if line_data[0] in good_titles_set: f.write(line)
+    outline += "\n"
+    f.write(outline)
+    if count_lines % 100 == 0: 
+        print(count_lines)
 
 
 f.close()
